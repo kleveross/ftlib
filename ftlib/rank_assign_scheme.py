1589623e599544cf94d3374c54f38a90cf63e343
Wang Zhang <zhangwang@caicloud.io>
2019-12-15 22:42:15 +0800

    fix: update rank-assign scheme

--- ftlib/rank_assign_scheme.py
+++ ftlib/rank_assign_scheme.py
@@ -2,11 +2,11 @@ def get_rank_size(member_list, self_identity):
     if self_identity not in member_list:
         return -1
 
-    hashed_member_list = [hash(m) for m in member_list]
-    hashed_member_list = sorted(hashed_member_list, key=lambda m: m[1])
+    hashed_member_dict = {hash(m): m for m in member_list}
+    hashed_member_list = sorted(hashed_member_dict.keys())
 
     return (
-        hashed_member_list.index((self_identity, hash(self_identity))),
+        hashed_member_list.index(hash(self_identity)),
         len(hashed_member_list),
-        hashed_member_list[0][0],
+        hashed_member_dict[hashed_member_list[0]],
     )
