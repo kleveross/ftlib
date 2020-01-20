1bc77034d4639406b0103aaa10da3ba72d7fc9c4
Wang Zhang <zhangwang@caicloud.io>
2020-01-20 17:09:28 +0800

    fix: update gossip test file

--- ftlib/consensus/gossip/test.py
+++ ftlib/consensus/gossip/test.py
@@ -1,7 +1,8 @@
+import time
 from ctypes import POINTER, Structure, c_char_p, c_int, c_longlong, cdll
 
 # loading shared object
-lib = cdll.LoadLibrary("memberlist.so")
+lib = cdll.LoadLibrary("/crystal/ftlib/ftlib/consensus/gossip/memberlist.so")
 
 
 # go type
@@ -20,18 +21,17 @@ class member_list(Structure):
 lib.join.argtypes = [GoSlice]
 lib.get_memberlist.restype = member_list
 
-t = (
-    (c_char_p * 3)(
-        c_char_p("192.168.22.1".encode("utf-8")),
-        c_char_p("192.168.22.2".encode("utf-8")),
-        c_char_p("192.168.22.3".encode("utf-8")),
-    ),
-    3,
-    3,
+t = GoSlice(
+    (c_char_p * 1)(c_char_p("ftlib-test-nrs12.default".encode("utf-8")),),
+    1,
+    1,
 )
 
-res = lib.init_memberlist("/tmp/memberlist.log")
+
+res = lib.init_memberlist("/tmp/memberlist.log".encode("utf-8"))
 print(res)
+
+time.sleep(60)
 
 res = lib.join(t)
 print(res)
@@ -39,3 +39,5 @@ print(res)
 ml = lib.get_memberlist()
 for i in range(ml.size):
     print(ml.addrs[i].decode("utf-8"))
+
+time.sleep(120)
